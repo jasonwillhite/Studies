6f3f4bed97bbe55b2859f52c3069b4a3a619cd3c
Jason Willhite <jason_willhite@hotmail.com>
2017-11-21 18:44:55 -0600

    Add Builder project

---
+++ DesignPatterns/DesignPatterns.Builder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.Builder
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
