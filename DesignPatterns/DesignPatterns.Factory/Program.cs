3be8f7ee882466ad961d263f7e1d6ef7df9766b8
Jason Willhite <jason_willhite@hotmail.com>
2017-12-19 21:21:31 -0600

    add shell for projects

---
+++ DesignPatterns/DesignPatterns.Factory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.Factory
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns.Prototype/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.Prototype
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
