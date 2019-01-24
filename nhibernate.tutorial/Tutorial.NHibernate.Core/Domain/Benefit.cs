41f63d46964de12bcbdeac3211cd7bbe3d19d773
Jason Willhite <Jason.Willhite@usablelife.com>
2019-01-24 16:51:13 -0600

    # 5 - remove cache and update gitignore

---
+++ nhibernate.tutorial/Tutorial.NHibernate.Core/Domain/Benefit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutorial.NHibernate.Core.Domain
+{
+    public class Benefit
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ nhibernate.tutorial/Tutorial.NHibernate.Core/Domain/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutorial.NHibernate.Core.Domain
+{
+    public class Employee
+    {
+        public string EmployeeNumber { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfJoining { get; set; }
+
+        public Address ResidentialAddress { get; set; }
+        public bool IsAdmin { get; set; }
+        public string Password { get; set; }
+        public ICollection<Benefit> Benefits { get; set; }
+    }
+}
